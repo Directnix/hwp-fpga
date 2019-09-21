1185558ace8243b2fa88778a2985758f1cfe10c2
Directnix <nick_vend95@hotmail.com>
2019-09-21 21:17:55 +0200

    added assignment 3

---
+++ modelsim/assignment3/calculator.vhd
@@ -0,0 +1,49 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all; 
+
+ENTITY calculator IS 
+	PORT (
+ 		SW_2, SW_1: in std_logic_vector(3 downto 0);	-- rename to sw range
+		BTN_0: in std_logic;
+		HEX_6, HEX_3, HEX_1, HEX_0: out std_logic_vector(6 downto 0)
+	);
+END calculator; 
+
+ARCHITECTURE calc OF calculator IS
+COMPONENT seven_segment is 
+port(
+	inputs: in std_logic_vector(3 downto 0);
+	segments: out std_logic_vector(6 downto 0)
+);
+end COMPONENT;
+SIGNAL hex_a_1, hex_a_0 : std_logic_vector(3 DOWNTO 0); 
+BEGIN
+
+hex_first: seven_segment PORT MAP (inputs => SW_2, segments => HEX_6);
+hex_second: seven_segment PORT MAP (inputs => SW_1, segments => HEX_3);
+
+hex_a_first: seven_segment PORT MAP (inputs => hex_a_1, segments => HEX_1);
+hex_a_second: seven_segment PORT MAP (inputs => hex_a_0, segments => HEX_0);
+
+PROCESS(BTN_0)
+variable sum: integer := 0;
+BEGIN
+	IF FALLING_EDGE(BTN_0) THEN
+		sum := to_integer(unsigned('0' & SW_1) + unsigned('0' & SW_2));
+		IF sum > 15 THEN
+			hex_a_1 <= "0001";
+			hex_a_0 <= std_logic_vector(to_unsigned(sum - 15, 4));
+		ELSE
+			hex_a_0 <= std_logic_vector(to_unsigned(sum, 4));	
+		END IF;	
+	END IF;
+END PROCESS;
+
+-- PROCESS(SW_1, SW_2)
+-- BEGIN
+--	hex_a_0 <= "UUUU";
+--	hex_a_1 <= "UUUU";
+-- END PROCESS;
+
+END calc;
---
+++ modelsim/assignment3/seven-segment_tb.vhd
@@ -0,0 +1,36 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+-- The entity of your testbench. No ports declaration in this case.
+ENTITY seven_segment_tb IS
+END ENTITY;
+ARCHITECTURE testbench OF seven_segment_tb IS
+ COMPONENT seven_segment IS
+port(
+	inputs: in std_logic_vector(3 downto 0);
+	segments: out std_logic_vector(6 downto 0)
+);
+ END COMPONENT;
+ -- Signal declaration. These signals are used to drive your inputs and
+ -- store results (if required).
+ SIGNAL SW_tb : STD_LOGIC_VECTOR(3 DOWNTO 0);
+ SIGNAL HEX0_tb : STD_LOGIC_VECTOR(6 DOWNTO 0);
+ BEGIN
+ -- A port map is in this case nothing more than a construction to
+-- connect your entity ports with your signals.
+tb: seven_segment PORT MAP (inputs => SW_tb, segments => HEX0_tb);
+PROCESS
+BEGIN
+ -- Initialize signals.
+ SW_tb <= "0000";
+ -- Loop through values.
+ FOR I IN 0 TO 15 LOOP
+ WAIT FOR 10 ns;
+ -- Increment by one.
+ SW_tb <= STD_LOGIC_VECTOR(UNSIGNED(SW_tb) + 1);
+ END LOOP;
+ REPORT "Test completed.";
+ -- Wait forever.
+ WAIT;
+END PROCESS;
+END ARCHITECTURE; 
